760274724d88ce1bbd8034be4c74266f336df730
Naomi H. Cordero <naomigpehdezcor@gmail.com>
2026-01-29 18:29:33 -0600

    Se agrega el archivo "tipos-de-datos,js" correspondiente a la actividad del día 1.

---
+++ tipos-de-datos.js
@@ -0,0 +1,65 @@
+let numero = 42;
+console.log(numero);
+console.log(typeof numero);
+
+let NumeroConLetra = 'Veinticinco';
+console.log(NumeroConLetra);
+console.log(typeof NumeroConLetra);
+
+let NumeroNegativo = -666;
+console.log(NumeroNegativo);
+console.log(typeof NumeroNegativo);
+
+let NacionalidadMexicana = true;
+console.log(NacionalidadMexicana);
+console.log(typeof NacionalidadMexicana);
+
+let cero = 0;
+console.log(cero);
+console.log(typeof cero);
+
+let comillas = '';
+console.log(comillas);
+console.log(typeof comillas);
+
+let PerfilDeFacebook = null;
+console.log(null);
+console.log(typeof null);
+
+let NumeroTelefonico= undefined;
+console.log(NumeroTelefonico);
+console.log(typeof NumeroTelefonico);
+
+let NacionalidadChina = FALSE;
+console.log(NacionalidadChina);
+console.log(typeof NacionalidadMexicana);
+
+//Agrega tus propios ejemplos y experimenta con algunos valores y sus tipos de datos.
+let NumeroDeMascotas = 2;
+console.log(NumeroDeMascotas);
+console.log(typeof NumeroDeMascotas);
+
+let TengoUnaGallina = false;
+console.log(TengoUnaGallina);
+console.log(typeof TengoUnaGallina);
+
+let TeniaUnaGallina = true;
+console.log(TeniaUnaGallina);
+console.log(typeof TeniaUnaGallina);
+
+let HamstersEnMiCasa = null;
+console.log(HamstersEnMiCasa);
+console.log(typeof HamstersEnMiCasa);
+
+let NumeroDeVecinos = undefined;
+console.log(NumeroDeVecinos);
+console.log(typeof NumeroDeVecinos);
+
+let mascotas = {
+    nombre1: 'Misty',
+    nombre2: 'Donatello Leonardo',
+    nombre3: null,
+    total: 2,
+};
+console.log(mascotas);
+console.log(typeof mascotas);
