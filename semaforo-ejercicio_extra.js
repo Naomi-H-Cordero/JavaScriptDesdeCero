d77c501b6d60810ebb740f84e8087745ef6ef402
Naomi H. Cordero <naomigpehdezcor@gmail.com>
2026-02-03 18:29:05 -0600

    Se agrega un archivo con un ejercicio extra de práctica.

---
+++ semaforo-ejercicio_extra.js
@@ -0,0 +1,14 @@
+/*Ejercicio extra de práctica
+Semáforo: Dado un color, se determina si es verde, amarillo o rojo.
+Según el color, se mostrará un mensaje personalizado.*/
+console.log ("Ejercicio extra de práctica (Semáforo)");
+let color = "verde";
+console.log (color);
+if (color === "verde"){
+    console.log("Avanza.")
+}else if(color === "amarillo"){
+    console.log("Precaución.")
+}
+else {(color === "rojo")
+    console.log("Detente.")
+}
